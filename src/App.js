d78bbd6d1d8aa9648f29c58eb73ff8f585d573f0
irfanizudin <irfanizzudyn@gmail.com>
2021-08-03 21:01:53 +0700

    creating functional component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const data = [
+  {
+    id: 1,
+    message: "Cari Makan"
+  },
+  {
+    id: 2,
+    message: "Belajar React"
+  },
+  {
+    id: 3,
+    message: "Belajar Vue"
+  }
+]
+
+const TaskAdder = () => {
+  return <div>
+    <input type="text" />
+    <button>Tambah</button>
+  </div>
 }
 
-export default App;
+const InfoBar = ({ taskNumber }) => {
+  return <div>Ada {taskNumber} yang perlu dikerjakan</div>
+}
+
+const Header = () => {
+  return <h1>React Todo</h1>
+}
+
+const Task = ({ id, message }) => {
+  return <div>
+    <div>{message}</div>
+    <button>Delete</button>
+  </div>
+}
+
+const TaskList = ({ tasks }) => {
+  return tasks.map(task => {
+    return <Task id={task.id} message={task.message} />
+  })
+}
+
+const TaskApp = () => {
+  return <>
+    <Header />
+    <InfoBar taskNumber={3} />
+    <TaskAdder />
+    <TaskList tasks={data} />
+  </>
+}
+
+const App = () => {
+  return (
+    <TaskApp />
+  )
+}
+
+export default App
